016bbfeb86e2477c15b29964b24d0796631d1738
vpulpatil <a8b.7_@vk2>
2017-09-22 00:54:12 +0530

    Configured Crashlytics, Resolved the issue of ripple effect not applied to viewgroups by removing android:onClick attribute

--- app/src/main/java/design/demo/rippledemo/MainActivity.java
+++ app/src/main/java/design/demo/rippledemo/MainActivity.java
@@ -3,11 +3,22 @@ package design.demo.rippledemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initializes Fabric for builds that don't use the debug build type.
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        Fabric.with(this, crashlyticsKit);
         setContentView(R.layout.activity_main);
     }
 }
